2c942fcd899f937eb9cbf623f0f3ad257b2c732c
Zackson Pessoa <zacksonpessoa@hotmail.com>
2021-04-21 13:01:50 -0300

    Project created

---
+++ src/main/java/com/zacksonpessoa/portifolio_javaEE_1/PortifolioJavaEe1Application.java
@@ -0,0 +1,13 @@
+package com.zacksonpessoa.portifolio_javaEE_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioJavaEe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioJavaEe1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zacksonpessoa/portifolio_javaEE_1/PortifolioJavaEe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacksonpessoa.portifolio_javaEE_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioJavaEe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
